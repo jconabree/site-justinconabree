3a9fde88a1aeb8d1301389095d770d17b90e8c99
Justin Conabree <jconabree454@gmail.com>
2024-03-31 15:32:49 -0400

    Adding content to about us section, adding pill styles

--- app-pages/Home/Sections/About/about.tsx
+++ app-pages/Home/Sections/About/about.tsx
@@ -1,5 +1,25 @@
+import classes from './about.module.css';
+
 export default function About() {
     return (
-        <div>About content</div>
+        <div className="h-full w-full flex items-center justify-center py-24">
+            <div className="flex flex-wrap gap-4 max-w-7xl px-10 items-center justify-center">
+                <div className={classes.attributeItem}>Full-stack Developer</div>
+                <div className={classes.attributeItem}>Problem Solver</div>
+                <div className={classes.attributeItem}>Highly Adaptive</div>
+                <div className={classes.attributeItem}>Constantly Learning</div>
+                <div className={classes.attributeItem}>Product Innovation</div>
+                <div className={classes.attributeItem}>Proactive</div>
+                <div className={classes.attributeItem}>Coding Standards</div>
+                <div className={classes.attributeItem}>Process Implementation</div>
+                <div className={classes.attributeItem}>Performance Analysis</div>
+                <div className={classes.attributeItem}>Code Optimization</div>
+                <div className={classes.attributeItem}>Team Player</div>
+                <div className={classes.attributeItem}>Leadership</div>
+                <div className={classes.attributeItem}>Agile Project Management</div>
+                <div className={classes.attributeItem}>Cross Functional Team Management</div>
+                <div className={classes.attributeItem}>Stakeholder Communications</div>
+            </div>
+        </div>
     );
 }
--- app-pages/Home/Sections/Hero/hero.tsx
+++ app-pages/Home/Sections/Hero/hero.tsx
@@ -2,8 +2,8 @@ export default function Hero() {
     return (
         <div className="h-full w-full relative">
             <div className="min-h-screen w-full bg-gray-100" />
-            <div className="absolute bottom-0 left-0 px-10">
-                <h1 className="title-h2">Web Developer</h1>
+            <div className="absolute bottom-0 left-0 px-10 max-w-1/2 pb-10">
+                <h1 className="title-hero leading-none">Web Developer</h1>
             </div>
         </div>
     )
