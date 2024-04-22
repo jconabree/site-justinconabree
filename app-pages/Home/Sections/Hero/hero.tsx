f9719583dd9bc6653518e7dda823146a75b0fa0d
Justin Conabree <jconabree@absolunet.com>
2024-04-22 14:19:21 -0400

    change hero bg to black for slow load

--- app-pages/Home/Sections/Hero/hero.tsx
+++ app-pages/Home/Sections/Hero/hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
         <>
             <div className="w-full relative min-h-screen rounded-bl-20 xl_rounded-bl-1/3 overflow-hidden">
                 <SplineScene
-                    className="w-full min-h-screen xl_min-h-none xl_aspect-4/3 pointer-events-none"
+                    className="w-full min-h-screen xl_min-h-none xl_aspect-4/3 pointer-events-none bg-black"
                     sceneUrl="https://prod.spline.design/V3aWT7tFTf9IuUM2/scene.splinecode"
                 />
                 <div className="absolute top-screen -translate-y-full left-0 px-10 w-full pb-10">
